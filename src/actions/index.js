cc937318ac5f904df93ac73235d48d817f1a3fe4
Lorena González Cordero <lore_383@hotmail.com>
2018-11-07 15:36:21 +0100

    chore(fetchWeatherCreator): Making the fetchWeatherCreator to get forecast API request.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
+import Axios from 'axios';
+
 const API_KEY = '307fb92d2597bf86e425807ff8c4efc1';
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url =  `${ROOT_URL}&q=${city},es`
+    const request = Axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
