2d7b3e81178045a38f55b7fc27bff886be8d26e9
Lorena González Cordero <lore_383@hotmail.com>
2018-11-08 16:44:59 +0100

    update(weatheList): Created a component to reuse sparklines graphics and DRY.

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
     renderWeather(cityData) {
         const name = cityData.city.name;
+        const temps = cityData.list.map(weather => weather.main.temp - 273.15);
+        const pressures = cityData.list.map(weather => weather.main.pressure);
+        const humidities = cityData.list.map(weather => weather.main.humidity);
+        
         return  (
             <tr key={ name }>
                 <td>{ name }</td>
-                <td>{ Math.trunc(cityData.list[0].main.temp - 273.15) + ' ºC' }</td>
-                <td>{ cityData.list[0].main.pressure + ' hpa'}</td>
-                <td>{ cityData.list[0].main.humidity + ' %' }</td>
+                <td><Chart data={ temps } color="red" units='ºC'/></td>
+                <td><Chart data={ pressures } color="black" units='hPa' /></td>
+                <td><Chart data={ humidities } color="blue" units='%' /></td>
             </tr>
         );
     }
@@ -20,9 +25,9 @@ class WeatherList extends Component {
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
+                        <th>Temperature (ºC)</th>
+                        <th>Pressure (hPa)</th>
+                        <th>Humidity (%)</th>
                     </tr>
                 </thead>
                 <tbody>
